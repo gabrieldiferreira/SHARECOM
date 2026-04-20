40983f172b2b9d3942dbb56c6efe225b69f289ac
Gabriel Ferreira <gabrieldiferreira.club@gmail.com>
2026-04-20 18:11:51 -0300

    db upgrade

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { Firestore, getFirestore } from "firebase/firestore";
 import { Analytics, getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYIuIphaTzqV56gwbWOHYShf5p-cyxYCk",
-  authDomain: "unidoc-493609.firebaseapp.com",
-  projectId: "unidoc-493609",
-  storageBucket: "unidoc-493609.firebasestorage.app",
-  messagingSenderId: "894636866610",
-  appId: "1:894636866610:web:ed5fcd475112f9e037502b",
-  measurementId: "G-JVY94D582R"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAYIuIphaTzqV56gwbWOHYShf5p-cyxYCk",
+  authDomain: "auth.sharecom.com.br",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "unidoc-493609",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "unidoc-493609.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "894636866610",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:894636866610:web:ed5fcd475112f9e037502b",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-JVY94D582R"
 };
 
 export const hasFirebaseConfig = [
